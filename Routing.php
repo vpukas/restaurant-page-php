5c25e5b646dbfae8fb4c4139ff634f92b681faea
Valerii Pukas <vpuk@atinea.pl>
2023-02-07 17:37:23 +0100

    Basic routing introduced

---
+++ Routing.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'src/controllers/DefaultController.php';
+class Routing {
+    public static $routes;
+
+    public static function get($url, $controller) {
+        self::$routes[$url] = $controller;
+    }
+
+    public static function run($url) {
+        $action = explode("/", $url)[0];
+        if(!array_key_exists($action, self::$routes)) {
+            die("Wrong url");
+        }
+
+        $controller = self::$routes[$action];
+        $object = new $controller;
+        $object->$action();
+    }
+}
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "Hello world";
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('login', 'DefaultController');
+Routing::get('index', 'DefaultController');
+
+Routing::run($path);
